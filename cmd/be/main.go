9bbc3cc68254504e41453ca656a2ed81f5d7c0c6
dharshatharan <dharshatharan@gmail.com>
2023-12-07 17:00:36 -0500

    Add basic loadbalancer that proxies to a backend server

---
+++ cmd/be/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"strconv"
+)
+
+func main() {
+
+	port := flag.Int("p", 8081, "port number")
+	flag.Parse()
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("Received request form %s\n", r.RemoteAddr)
+		log.Printf("%s %s %s\n", r.Method, r.URL.Path, r.Proto)
+		log.Printf("Host: %s\n", r.Host)
+		log.Printf("User-Agent: %s\n", r.UserAgent())
+		log.Printf("Accept: %s\n", r.Header["Accept"])
+
+		fmt.Fprintf(w, "Hello from backend server!")
+		log.Printf("Replied with a hello message\n")
+	})
+
+	log.Fatal(http.ListenAndServe(":"+strconv.Itoa(*port), nil))
+
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/dharshatharan/go-load-balancer
+
+go 1.21.4
